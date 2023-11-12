77cc333396e0e88d516fdd53c8a5977c4115dbe1
Bryce Mecum <petridish@gmail.com>
2023-11-12 11:10:00 -0900

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<>
 			<Head>
-				<title>System Status</title>
+				<title>TreeStats Service Status</title>
 			</Head>
 			<Component {...pageProps} />
 		</>
