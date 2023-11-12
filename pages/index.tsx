4c776efc43c75405dcdf58d5f396028902ded441
Bryce Mecum <petridish@gmail.com>
2023-11-12 11:05:18 -0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import ServicesSection from "../src/services"
 const Home: NextPage = () => {
   return (
     <div className='h-full w-full '>
-      <div className="mt-20 absolute inset-0 bg-[url(/grid.svg)] bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]"></div>
+      <div className="mt-20 absolute inset-0 bg-[url(/./grid.svg)] bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]"></div>
       <div className="w-full h-20 absolute dark:purple dark:bg-black">
         <div className="sm:ml-0 ml-5 mr-0 mt-3 md:pl-80 md:pr-80 sm:w-full h-full bg-purple-500 dark:bg-black text-center text-lg font-bold">
          TreeStats Service Status
